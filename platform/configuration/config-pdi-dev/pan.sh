54f08997f70896fca47ea90a8efd445948ed0479
Beppe Raymaekers <beppe.raymaekers@gmail.com>
2019-02-28 18:05:19 +0100

    removed submodule

--- platform/configuration/config-pdi-dev/pan.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-export PENTAHO_ENV=config-pdi-local
-cd "$PWD"/../bin/
-sh pan.sh
--- platform/configuration/config-pdi-dev/start-pentaho.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-export PENTAHO_ENV=config-pdi-local
-cd "$PWD"/../bin/
-sh start-pentaho.sh
